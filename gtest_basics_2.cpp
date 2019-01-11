939bf8c8d937cbcaa8c9dca6572df037533ca3ef
Liam Bucci <controls.freq@gmail.com>
2019-01-10 19:23:39 -0800

    Update gtest_basics_2.cpp

--- gtest_basics_2.cpp
+++ gtest_basics_2.cpp
@@ -1,2 +1,16 @@
 TEST_F(MyUutTest, AssertExamples) {
+  ASSERT_EQ(1U, 0U); // Fails immediately
+  EXPECT_EQ(1U, 0U); // Marks test as failed but continues
+  
+  // Use these when you're checking a boolean value
+  ASSERT_TRUE(uut.isTrue());
+  ASSERT_FALSE(uut.isFalse());
+  
+  // Use these when you are comparing two objects
+  ASSERT_EQ(1, uut.getInt());
+  ASSERT_NE(1, uut.getInt());
+  ASSERT_LT(1, uut.getInt());
+  ASSERT_LE(1, uut.getInt());
+  ASSERT_GT(1, uut.getInt());
+  ASSERT_GE(1, uut.getInt());
 }
